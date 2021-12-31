d3f381b1dd0a812b940ec240dd7836bad17179aa
Cradoe <me@ismailobadimu.com>
2021-12-31 07:18:49 +0100

    Modified quote card

--- src/components/ShareButton/index.js
+++ src/components/ShareButton/index.js
@@ -23,7 +23,7 @@ export default function ShareButton ( { content } ) {
                 leaveFrom="transform opacity-100 scale-100"
                 leaveTo="transform opacity-0 scale-95"
             >
-                <Menu.Items className="origin-top-right absolute right-0 mt-2 w-56 rounded-md shadow-lg bg-white ring-1 ring-black ring-opacity-5 divide-y divide-gray-100 focus:outline-none z-40">
+                <Menu.Items className="origin-top-right absolute bottom-2 right-12 mt-2 w-40 max-w-56 md:w-56 rounded-md shadow-lg bg-white ring-1 ring-black ring-opacity-5 divide-y divide-gray-100 focus:outline-none z-40" style={{ zIndex: '9999 !important' }}>
                     <div className="py-1 md:hidden ">
                         <Menu.Item>
                             {( { active } ) => (
